32adc20de1329eee258fe76e66d896f154474385
Nick <nicholasevans17@gmail.com>
2020-02-02 10:38:08 -0800

    remove secrets.py

--- restaurant/secrets.py
+++ restaurant/secrets.py
@@ -2,6 +2,7 @@ secret_key = '6i#r734(=9_o%#jn96j6$dk&i68sk)g9=@r(5w#@!axn!9uafc'
 
 stripe_key = 'sk_test_4GWJaRirO2jEORvFjNHi5hvZ00p1DiqyX7'
 
-aws_access_key_id = 'AKIAWYWMNNP3IUQH4TNM'
+aws_access_key_id = ''
 
-aws_secret_access_key = 'CnmtUKB0w3P7mx3qoqby2AaVxrE8vHeqlkRud+EX'
+aws_secret_access_key = ''
+
